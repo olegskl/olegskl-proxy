4ffaba92e70cb2c6d81fdb00db6a468a5cbc5e38
Oleg Sklyanchuk (oregu) <sklyanchuk@gmail.com>
2013-06-24 13:37:15 +0200

    Initial commit

---
+++ proxy.js
@@ -0,0 +1,31 @@
+/* Proxy server for oregu.com */
+/*jslint node: true */
+'use strict';
+
+var fs = require('fs'),
+    util = require('util'),
+    httpProxy = require('http-proxy'),
+    httpProxyOptionsFilePath = 'http-proxy-options.json',
+    httpProxyPort = 80;
+
+util.print('Reading proxy options file... ');
+fs.readFile(httpProxyOptionsFilePath, 'utf-8', function (error, contents) {
+    if (error) {
+        util.print('\x1b[31mfailed\x1b[0m.\n');
+        return;
+    }
+    util.print('\x1b[32mdone\x1b[0m.\n');
+
+    // Establish the proxy server with given options on port 80:
+    httpProxy.createServer(function (request, response, proxy) {
+        // Redirect sklyanchuk.com to oregu.com:
+        if (request.headers.host === 'sklyanchuk.com') {
+            response.writeHead(302, {'Location': 'http://oregu.com'});
+            response.end('Redirecting to http://oregu.com ...');
+        } else {
+            proxy.proxyRequest(request, response, JSON.parse(contents));
+        }
+    }).listen(httpProxyPort);
+
+    console.log('Proxy server running on port ' + httpProxyPort + '.');
+});
