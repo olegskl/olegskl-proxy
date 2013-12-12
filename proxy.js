5b28ee87aec530b2d9d4751dd836ea46ba59c5bc
Oleg Sklyanchuk (oregu) <sklyanchuk@gmail.com>
2013-12-12 15:55:56 +0100

    Restructure for clustering and simplicity.

--- proxy.js
+++ proxy.js
@@ -1,33 +1,29 @@
 /* Proxy server for oregu.com */
+
 /*jslint node: true */
 'use strict';
 
-var fs = require('fs'),
-    http = require('http'),
-    util = require('util'),
-    path = require('path'),
-    httpProxy = require('http-proxy'),
-    httpProxyOptionsFilePath = path.join(__dirname, 'http-proxy-options.json'),
+var httpProxy = require('http-proxy'),
+    httpProxyOptions = {
+        hostnameOnly: true,
+        router: {'oregu.com': '127.0.0.1:8000'}
+    },
+    redirectThese = ['sklyanchuk.com', 'ridetojapan.com'],
     httpProxyPort = 80;
 
-util.print('Reading proxy options file... ');
-fs.readFile(httpProxyOptionsFilePath, 'utf-8', function (error, contents) {
-    if (error) {
-        util.print('\x1b[31mfailed\x1b[0m.\n');
-        return;
+// Custom server redirect logic before proxying requests.
+function redirectOrProxy(request, response, proxy) {
+    // Redirect sklyanchuk.com to oregu.com:
+    if (redirectThese.indexOf(request.headers.host) !== -1) {
+        response.writeHead(302, {'Location': 'http://oregu.com'});
+        response.end('Redirecting to http://oregu.com ...');
+    } else {
+        proxy.proxyRequest(request, response);
     }
-    util.print('\x1b[32mdone\x1b[0m.\n');
+}
 
-    // Establish the proxy server with given options on port 80:
-    httpProxy.createServer(function (request, response, proxy) {
-        // Redirect sklyanchuk.com to oregu.com:
-        if (request.headers.host === 'sklyanchuk.com') {
-            response.writeHead(302, {'Location': 'http://oregu.com'});
-            response.end('Redirecting to http://oregu.com ...');
-        } else {
-            proxy.proxyRequest(request, response);
-        }
-    }, JSON.parse(contents)).listen(httpProxyPort);
-
-    console.log('Proxy server running on port ' + httpProxyPort + '.');
-});
+// Establish the proxy server with given options,
+// and export it for clustering purposes:
+module.exports = httpProxy
+    .createServer(redirectOrProxy, httpProxyOptions)
+    .listen(httpProxyPort);
