d6e36a7659cdea67e07a84766c6ef298bb3baab9
Oleg Sklyanchuk (oregu) <sklyanchuk@gmail.com>
2014-08-26 12:18:08 +0200

    Redirect to olegskl.com instead of oregu.com

--- proxy.js
+++ proxy.js
@@ -1,4 +1,4 @@
-/* Proxy server for oregu.com */
+/* Proxy server for olegskl.com */
 
 /*jslint node: true */
 'use strict';
@@ -6,18 +6,18 @@
 var httpProxy = require('http-proxy'),
     httpProxyOptions = {
         hostnameOnly: true,
-        router: {'oregu.com': '127.0.0.1:8000'}
+        router: {'olegskl.com': '127.0.0.1:8000'}
     },
     port = 80;
 
 // Custom server redirect logic before proxying requests.
 function redirectOrProxy(request, response, proxy) {
-    // Redirect sklyanchuk.com to oregu.com:
-    if (request.headers.host === 'oregu.com') {
+    if (request.headers.host === 'olegskl.com') {
         proxy.proxyRequest(request, response);
     } else {
-        response.writeHead(302, {'Location': 'http://oregu.com'});
-        response.end('Redirecting to http://oregu.com ...');
+        // Redirect other domains to olegskl.com:
+        response.writeHead(302, {'Location': 'http://olegskl.com'});
+        response.end('Redirecting to http://olegskl.com ...');
     }
 }
 
