5e9b63dce335aeb3efbf0cf25efa20c842abe564
olegskl <sklyanchuk@gmail.com>
2015-08-06 13:12:52 +0200

    feat(config): add arrayres subdomains

--- config.js
+++ config.js
@@ -4,6 +4,9 @@
 module.exports = {
     proxyPort: 80,
     proxyRoutesConfig: {
-        'olegskl.com': 'http://127.0.0.1:8000'
+        'olegskl.com': 'http://127.0.0.1:8000',
+        'arrayres.olegskl.com': 'http://127.0.0.1:8001',
+        'arrayres.beta.olegskl.com': 'http://127.0.0.1:8002',
+        'arrayres.test.olegskl.com': 'http://127.0.0.1:8003'
     }
 };
