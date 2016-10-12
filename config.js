5c6e962f74fac9bcb12ae52428b355ecf053a8a9
Oleg SKLYANCHUK <sklyanchuk@gmail.com>
2016-10-12 10:35:45 +0200

    feat(config): remove arrayres submdomains

--- config.js
+++ config.js
@@ -4,9 +4,6 @@
 module.exports = {
     proxyPort: 80,
     proxyRoutesConfig: {
-        'olegskl.com': 'http://127.0.0.1:8000',
-        'arrayres.olegskl.com': 'http://127.0.0.1:8001',
-        'arrayres.beta.olegskl.com': 'http://127.0.0.1:8002',
-        'arrayres.test.olegskl.com': 'http://127.0.0.1:8003'
+        'olegskl.com': 'http://127.0.0.1:8000'
     }
 };
