b45f33b0fbe952a6702a4cd44e24d055c2e0686c
Oleg SKLYANCHUK <sklyanchuk@gmail.com>
2016-10-13 10:56:18 +0200

    refactor(proxy): rewrite in ES6

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
-/*jslint node: true */
-'use strict';
-
 module.exports = {
-    proxyPort: 80,
-    proxyRoutesConfig: {
-        'olegskl.com': 'http://127.0.0.1:8000'
-    }
+  proxyPort: 80,
+  proxyRoutesConfig: {
+    'olegskl.com': 'http://127.0.0.1:8000'
+  }
 };
--- proxy.js
+++ proxy.js
@@ -1,25 +1,22 @@
-/*jslint node: true */
-'use strict';
-
-var http = require('http'),
-    httpProxy = require('http-proxy'),
-    config = require('./config'),
-    proxy = httpProxy.createProxyServer({});
+const http = require('http');
+const httpProxy = require('http-proxy');
+const config = require('./config');
+const proxy = httpProxy.createProxyServer({});
 
 // Custom server redirect logic before proxying requests.
 function redirectOrProxy(request, response) {
-    var target = config.proxyRoutesConfig[request.headers.host];
-    if (target) {
-        proxy.web(request, response, {target: target});
-    } else {
-        // Redirect other domains to olegskl.com:
-        response.writeHead(302, {'Location': 'http://olegskl.com'});
-        response.end('Redirecting to http://olegskl.com ...');
-    }
+  const target = config.proxyRoutesConfig[request.headers.host];
+  if (target) {
+    proxy.web(request, response, {target});
+  } else {
+    // Redirect other domains to olegskl.com:
+    response.writeHead(302, {Location: 'http://olegskl.com'});
+    response.end('Redirecting to http://olegskl.com ...');
+  }
 }
 
 // Establish the proxy server with given options,
 // and export it for clustering purposes:
 module.exports = http
-    .createServer(redirectOrProxy)
-    .listen(config.proxyPort);
+  .createServer(redirectOrProxy)
+  .listen(config.proxyPort);
